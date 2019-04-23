8d32204280ee8d3fa415832cf0b168ee63827d62
Coder <9761264+devinsg@users.noreply.github.com>
2019-04-23 16:24:34 +0000

    merge Develop => Master (#3)
    * init develop branch
    
    * fixed logo image (#2)

--- src/components/LeftDrawer.js
+++ src/components/LeftDrawer.js
@@ -6,6 +6,8 @@ import MenuItem from 'material-ui/MenuItem';
 import {Link} from 'react-router';
 import Avatar from 'material-ui/Avatar';
 
+const userImage = '../images/devinsg.png';
+
 const LeftDrawer = (props) => {
   let { navDrawerOpen } = props;
 
@@ -54,7 +56,7 @@ const LeftDrawer = (props) => {
           Material Admin
         </div>
         <div style={styles.avatar.div}>
-          <Avatar src="http://www.material-ui.com/images/uxceo-128.jpg"
+          <Avatar src={userImage}
                   size={50}
                   style={styles.avatar.icon}/>
           <span style={styles.avatar.span}>{props.username}</span>
