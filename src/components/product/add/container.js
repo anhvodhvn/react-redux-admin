a3ded99cb3a57d2186c40cc2468b15f83b5b57c3
devinsg <duyanh2005@gmail.com>
2019-04-24 09:37:43 +0000

    define component product: Add + Edit

---
+++ src/components/product/add/container.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ProductAdd extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div> Product Add New</div>
+        );
+    }
+}
+
+export default ProductAdd;
---
+++ src/components/product/edit/container.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ProductEdit extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div> Product Edit</div>
+        );
+    }
+}
+
+export default ProductEdit;
