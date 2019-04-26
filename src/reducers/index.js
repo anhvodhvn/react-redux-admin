9a37effa38ce0ea3ade7507b5e3cbf4a10b1ddfc
devinsg <duyanh2005@gmail.com>
2019-04-26 14:25:57 +0000

    wrap redux-form reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as reduxFormReducer } from 'redux-form';
 import productReducer from './productReducer';
 import merchantReducer from './merchantReducer';
 
 const rootReducer = combineReducers({
+    form: reduxFormReducer,
     productReducer,
     merchantReducer
 });
