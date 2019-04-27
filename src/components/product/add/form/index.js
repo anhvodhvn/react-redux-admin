60ca33437147de159bd0144ac8e46742e6acfe5a
devinsg <duyanh2005@gmail.com>
2019-04-27 08:55:16 +0000

    clean up

--- src/components/product/add/form/index.js
+++ src/components/product/add/form/index.js
@@ -6,9 +6,6 @@ import RaisedButton from 'material-ui/RaisedButton';
 import MenuItem from 'material-ui/MenuItem';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
-import Toggle from 'material-ui/Toggle';
-import DatePicker from 'material-ui/DatePicker';
-import Divider from 'material-ui/Divider';
 
 import validate from './validate';
 
@@ -23,6 +20,12 @@ const renderTextField = ({ input, label, meta: { touched, error }, ...custom }) 
                 {...custom} />
 );
 
+renderTextField.propTypes = {
+    input: PropTypes.object,
+    label: PropTypes.string,
+    meta: PropTypes.object
+};
+
 const renderSelectField = ({ input, label, meta: { touched, error }, children, ...custom }) => (
     <SelectField  floatingLabelText={label}
                   fullWidth={true}
@@ -32,6 +35,13 @@ const renderSelectField = ({ input, label, meta: { touched, error }, children, .
                   children={children}
                   {...custom} />
 );
+
+renderSelectField.propTypes = {
+    input: PropTypes.object,
+    label: PropTypes.string,
+    meta: PropTypes.object,
+    children: PropTypes.object
+};
 
 const ProductAddForm = (props) => {
     const { handleSubmit, locationList, categoryList } = props;
@@ -47,15 +57,7 @@ const ProductAddForm = (props) => {
                 { categoryList.map((item) => <MenuItem key={item.Code} value={item.Code} primaryText={item.Name}/>) }
             </Field>
 
-            <DatePicker hintText="Expiration Date" floatingLabelText="Expiration Date" fullWidth={true} />
-
             <Field name="price" component={renderTextField} label="Price"/>
-
-            <div style={styles.toggleDiv}>
-                <Toggle label="Disabled" labelStyle={styles.toggleLabel} />
-            </div>
-
-            <Divider/>
 
             <div style={styles.buttons}>
                 <Link to="/product">
