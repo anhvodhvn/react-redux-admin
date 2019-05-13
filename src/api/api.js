c52290820522312544f7d6dab65e0630b3029fd7
devinsg <duyanh2005@gmail.com>
2019-05-13 04:31:34 +0000

    tweak node_env

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import CONSTANTS from '../utils/constants';
 
 let API_URL = '';
-if (process.env.NODE_ENV == 'production') {
+if (CONSTANTS.PROCESS.NODE_ENV == 'production') {
     API_URL = 'https://78n3id557f.execute-api.us-east-1.amazonaws.com/api/';
 }
 else {
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,6 @@
-if(process.env.NODE_ENV === 'production') {
+import CONSTANTS from '../utils/constants';
+
+if(CONSTANTS.PROCESS.NODE_ENV == 'production') {
     module.exports = require('./configureStore.prod');
 }
 else {
