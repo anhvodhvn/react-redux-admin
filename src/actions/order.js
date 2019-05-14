157f35395ac22074c2cb23bf05654c7f7743eac2
devinsg <duyanh2005@gmail.com>
2019-05-14 09:33:01 +0000

    tweak more action definition

---
+++ src/actions/order.js
@@ -0,0 +1,11 @@
+//import api from '../api/api';
+//import CONSTANTS from '../utils/constants';
+//const { GET_MERCHANT_LIST, GET_ORDER_ITEM } = CONSTANTS;
+
+export const getOrderList = function() {
+    return [];
+};
+
+export const getOrderItem = function(id) {
+    return {id};
+};
