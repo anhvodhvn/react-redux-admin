75c0cfd2dcd0f5e2b7c04422660f080dfade13e2
devinsg <duyanh2005@gmail.com>
2019-05-14 10:26:08 +0000

    update reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import { reducer as reduxFormReducer } from 'redux-form';
 import productReducer from './productReducer';
 import merchantReducer from './merchantReducer';
-import loadingReducer from '../components/base/Loading/reducer';
+import loadingReducer from './loadingReducer';
 
 const rootReducer = combineReducers({
     form: reduxFormReducer,
