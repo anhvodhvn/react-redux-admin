7243e6dc61a615d275f719005e377a128c9a4cd8
devinsg <duyanh2005@gmail.com>
2019-05-14 04:48:33 +0000

    define Loading component

--- src/components/base/Loading/index.js
+++ src/components/base/Loading/index.js
@@ -7,8 +7,10 @@ class Loading extends Component {
   
   render() {
     return (
-      <div>
-        loading ...
+      <div className="spinner-loading">
+        <svg className="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+          <circle className="path" fill="none" cx="33" cy="33" r="30" />
+        </svg>
       </div>
     );
   }
--- src/containers/ProductPage.js
+++ src/containers/ProductPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Loading from '../components/base/Loading';
 import PageBase from '../components/PageBase';
 import Products from '../components/product';
 
 const ProductPage = () => {
   return (
     <PageBase title="Product List" navigation="Application / Product Page">
+      <Loading />
       <Products />
     </PageBase>
   );
