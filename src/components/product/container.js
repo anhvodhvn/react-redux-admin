672e7336549bece1f314981c9d1314c06917081d
devinsg <duyanh2005@gmail.com>
2019-05-20 16:21:40 +0000

    tweak Sequential & ProductId

--- src/components/product/container.js
+++ src/components/product/container.js
@@ -39,11 +39,12 @@ class Products extends Component {
                 <Table>
                     <TableHeader>
                         <TableRow>
-                        <TableHeaderColumn style={styles.columns.id}>ID</TableHeaderColumn>
-                        <TableHeaderColumn style={styles.columns.name}>Name</TableHeaderColumn>
-                        <TableHeaderColumn style={styles.columns.price}>Price</TableHeaderColumn>
-                        <TableHeaderColumn style={styles.columns.category}>Category</TableHeaderColumn>
-                        <TableHeaderColumn style={styles.columns.edit}>Edit</TableHeaderColumn>
+                            <TableHeaderColumn style={styles.columns.id}>No.</TableHeaderColumn>
+                            <TableHeaderColumn style={styles.columns.code}>ID</TableHeaderColumn>
+                            <TableHeaderColumn style={styles.columns.name}>Name</TableHeaderColumn>
+                            <TableHeaderColumn style={styles.columns.price}>Price</TableHeaderColumn>
+                            <TableHeaderColumn style={styles.columns.category}>Category</TableHeaderColumn>
+                            <TableHeaderColumn style={styles.columns.edit}>Edit</TableHeaderColumn>
                         </TableRow>
                     </TableHeader>
                     <TableBody>
@@ -52,6 +53,7 @@ class Products extends Component {
                             products.map((item, index) =>
                                 <TableRow key={index+1}>
                                     <TableRowColumn style={styles.columns.id}>{index+1}</TableRowColumn>
+                                    <TableRowColumn style={styles.columns.code}>{item.ProductId}</TableRowColumn>
                                     <TableRowColumn style={styles.columns.name}>{item.ProductName}</TableRowColumn>
                                     <TableRowColumn style={styles.columns.price}>{item.Price}</TableRowColumn>
                                     <TableRowColumn style={styles.columns.category}>{item.Category ? item.Category.Name : ''}</TableRowColumn>
