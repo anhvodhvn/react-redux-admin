6e275713c0f13a0546e03d4fb941c3ff7aaa7a85
devinsg <duyanh2005@gmail.com>
2019-05-22 09:26:05 +0000

    init order reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import { reducer as reduxFormReducer } from 'redux-form';
+import orderReducer from './orderReducer';
 import productReducer from './productReducer';
 import merchantReducer from './merchantReducer';
 import loadingReducer from './loadingReducer';
 
 const rootReducer = combineReducers({
     form: reduxFormReducer,
+    orderReducer,
     productReducer,
     merchantReducer,
     loadingReducer,
