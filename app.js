31c6fe95c02c79c95d9d67762cc57b8deace64f1
devinsg <duyanh2005@gmail.com>
2019-07-05 06:09:49 +0000

    fixed express version

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ server.set('port', process.env.PORT || 8080);
 
 server.use(express.static('dist'));
 server.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + './index.html'));
 });
 
 http.createServer(server).listen(server.get('port'), function () {
