1f0cabddf29deb8e395b11d7a968d164a14638c8
devinsg <duyanh2005@gmail.com>
2019-07-05 06:14:45 +0000

    fixed route path

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ server.set('port', process.env.PORT || 8080);
 
 const webpath = path.join(__dirname, 'dist');
 server.use('/', express.static(webpath, { index: 'index.html' }));
+server.get('*', function (req, res) {
+    res.sendFile(path.join(webpath, 'index.html'));
+});
 
 http.createServer(server).listen(server.get('port'), function () {
     console.log('Material Design UI is running on port:' + server.get('port'));
