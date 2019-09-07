b991ede1438ea662745820fe866cb84ac736bf15
devinsg <duyanh2005@gmail.com>
2019-09-07 22:19:26 +0700

    - clean up

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const path = require("path");
 const dist = path.join(__dirname, 'dist');
 server.get('/status', (req, res) => { res.send('Hello: ReactJs + NodeJs on Azure'); });
 server.use('/', express.static(dist, { index: 'index.html' }));
-// server.get('*', function (req, res) {
-//     res.sendFile(path.join(dist, 'index.html'));
-// });
 
 const port = process.env.PORT || 80;
 http.createServer(server).listen(port, function () {
