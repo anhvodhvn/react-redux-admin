4e0fa5da37d9646fa0cea21ea0888cb6e61760d2
devinsg <duyanh2005@gmail.com>
2019-09-07 23:08:46 +0700

    - deploy aws ec2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const dist = path.join(__dirname, 'dist');
 server.get('/status', (req, res) => { res.send('Hello: ReactJs + NodeJs on Azure'); });
 server.use('/', express.static(dist, { index: 'index.html' }));
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 http.createServer(server).listen(port, function () {
     console.log('Material Design UI is running on port:' + port);
 });
