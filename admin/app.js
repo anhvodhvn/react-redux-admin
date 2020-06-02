23a848c1f6f72cc57b98f54d9ae4e16be82c5570
devinsg <duyanh2005@gmail.com>
2020-06-02 23:15:59 +0700

    - merge FE & clean up files

--- admin/app.js
+++ admin/app.js
@@ -1,5 +1,12 @@
 const http = require('http');
-const server = require('./server');
+const express = require('express');
+const server = express();
+
+const path = require('path');
+const dist = path.join(__dirname, 'dist');
+
+server.get('/status', (req, res) => { res.send('Hello: ReactJs + NodeJs on Azure'); });
+server.use('/', express.static(dist, { index: 'index.html' }));
 
 const port = process.env.PORT || 8080;
 http.createServer(server).listen(port, function () {
