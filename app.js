74446de995f7beb893b7d12808972251cd4a8df4
shadow <9761264+devinsg@users.noreply.github.com>
2020-06-02 22:23:45 +0700

    FE release2 (#53)
    * - make build & release
    
    * - tweak release package

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-const express = require('express');
-const server = express();
 const http = require('http');
-const path = require("path");
-
-const dist = path.join(__dirname, 'dist');
-server.get('/status', (req, res) => { res.send('Hello: ReactJs + NodeJs on Azure'); });
-server.use('/', express.static(dist, { index: 'index.html' }));
+const server = require('./server');
 
 const port = process.env.PORT || 8080;
 http.createServer(server).listen(port, function () {
+    // eslint-disable-next-line no-console
     console.log('Material Design UI is running on port:' + port);
 });
