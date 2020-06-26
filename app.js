28c5354feb5c47c8c2d23174c58f96c9e8b647c1
Anh <voduyanh1984@gmail.com>
2020-06-26 13:55:42 +0700

    - clean information

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const HOST = '0.0.0.0';
 
 const port = process.env.PORT || PORT;
 const host = process.env.HOST || HOST;
+
 http.createServer(server).listen(port, host, function () {
     // eslint-disable-next-line no-console
     console.log(`Sale System is running on port: ${port} at ${host}`);
