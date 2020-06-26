aff179fc5201ffb078ae2a17c861e5127ce43240
Anh <voduyanh1984@gmail.com>
2020-06-26 11:55:08 +0700

    - refractor code

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const server = express();
+const path = require('path');
 const appInfo = require('./package.json');
 
-server.get('/status', (req, res) => { res.send({ 
-    success: true, 
-    version: appInfo.version 
-}); });
+server.use('/api/status', (req, res, next) => { 
+    return res.send({ 
+        success: true,
+        message: 'welcome friend',
+        version: appInfo.version 
+    });
+});
 
-const path = require('path');
 const dist = path.join(__dirname, './admin/dist');
-server.use('/', express.static(dist, { index: 'index.html' }));
+server.use(['/', '*'], express.static(dist, { index: 'index.html' }));
 
 module.exports = server;
